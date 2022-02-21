5c8cb29d6fdb582885a13bd126c435fbfb991a78
Guille-Dimi <guillecalvo81@gmail.com>
2022-02-21 14:25:22 -0300

    creación función para validar regalo
    cree una función para validar la descripción regalo según la consigna

--- clase7tarea..js
+++ clase7tarea..js
@@ -5,3 +5,16 @@ function validarCiudad(ciudad){
     return ''
 }
 
+function validarRegalo(regalo){
+    if(regalo.length === 0){
+        return 'Debe describir un regalo a recibir'
+    }
+    if(regalo.length >= 100){
+        return 'Debe de describir el regalo con menos de 100 caracteres'
+    }
+    if(!/^[a-z0-9 .,]+$/i.test(regalo)){
+        return 'Debe describir el regalo con solo letras y numeros'
+    }
+    return ''
+}
+
