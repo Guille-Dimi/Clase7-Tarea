528491dbf03ea8f65839f0a6889c34fe3db85f39
Guille-Dimi <guillecalvo81@gmail.com>
2022-02-21 14:30:03 -0300

    Creación función para validar funcionamiento correcto validar regalo
    Cree una función para validar el correcto funcionamiento para validar regalo según consigna

--- pruebasclase7tarea.js
+++ pruebasclase7tarea.js
@@ -10,3 +10,25 @@ function probarValidarCiudad(){
 }
 
 probarValidarCiudad()
+
+function probarValidarRegalo(){
+    console.assert(
+        validarRegalo('') === 'Debe describir un regalo a recibir',
+        'Validar regalo no validó que el usuario no escribiera nada',
+    )
+    console.assert(
+        validarRegalo('asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf') ===
+        'Debe de describir el regalo con menos de 100 caracteres',
+        'Validar regalo no validó que el usuario escribiera un regalo de mas de 100 caracteres'
+    )
+    console.assert(
+        validarRegalo('Un pinche helicoptero con champagne') === '',
+        'Validar regalo no validó correctamente un regalo valido'
+    )
+    console.assert(
+        validarRegalo('$_dsa+') === 'Debe describir el regalo con solo letras y numeros',
+        'Validar regalo no validó que solo tuviera letras y numeros'
+    )
+}
+
+probarValidarRegalo()
